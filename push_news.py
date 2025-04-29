3c6b01e1b85fee1e19808fb038783b3b6a946526
alexlin0413 <alex_lin0413@yahoo.com.tw>
2025-04-29 11:02:04 +0800

    Update push_news.py

--- push_news.py
+++ push_news.py
@@ -1,6 +1,6 @@
 import requests
 
-# 傳送訊息到 LINE
+# 發送訊息到 LINE
 def push_to_line(access_token, message):
     url = 'https://api.line.me/v2/bot/message/broadcast'
     headers = {
@@ -15,7 +15,6 @@ def push_to_line(access_token, message):
             }
         ]
     }
-
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code == 200:
@@ -26,9 +25,9 @@ def push_to_line(access_token, message):
 
 # 主程式
 if __name__ == "__main__":
+    ACCESS_TOKEN = "qrr1pB8Lt9s6mGs1r9E7SqU7QI0SucHAO6GtcbFtT9ulE2Ha0xLdwj/rBFkq9LnRJxriB5Hvl/QUkSIqCLHq9foGa2zwEyZNinars+mrzFuZq3qAFwuSMw1Hy4N4SobFNfq3ZD+wluVstG2+ICr/OwdB04t89/1O/w1cDnyilFU="
+    MESSAGE = "今日房市新聞更新完成，點擊查看詳情！"
 
-    ACCESS_TOKEN = 'qrr1pB8Lt9s6mGs1r9E7SqU7QI0SucHAO6GtcbFtT9ulE2Ha0xLdwj/rBFkq9LnRJxriB5Hvl/QUkSIqCLHq9foGa2zwEyZNinars+mrzFuZq3qAFwuSMw1Hy4N4SobFNfq3ZD+wluVstG2+ICr/OwdB04t89/1O/w1cDnyilFU='
-
-    MESSAGE = '今天的房市新聞更新完成，點擊查看詳情！'
-
+    # 呼叫推送函式
     push_to_line(ACCESS_TOKEN, MESSAGE)
+
